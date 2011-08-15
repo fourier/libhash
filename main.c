013922f5a82c86948fd32762aeb413ade0b00f3b
Alexey Veretennikov <txm.fourier@gmail.com>
2011-08-15 22:47:48 +0200

    Added copyright information

--- main.c
+++ main.c
@@ -1,3 +1,23 @@
+/* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ Copyright (C) 2011 Alexey Veretennikov (alexey dot veretennikov at gmail.com)
+ 
+	This file is part of libhash.
+
+ libhash is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published
+ by the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ libhash is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with libhash.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 #include <math.h>
 #include "libhash.h"
